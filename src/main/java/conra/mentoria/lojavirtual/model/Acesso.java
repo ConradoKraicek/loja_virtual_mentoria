d38ba5d9ad091013344edb807aa516dd17988708
Conrado A. Cardozo <cacardoso@tjba.jus.br>
2021-11-22 12:12:10 -0300

    Implementacao classes de modelo mentoria

---
+++ src/main/java/conra/mentoria/lojavirtual/model/Acesso.java
@@ -0,0 +1,31 @@
+package conra.mentoria.lojavirtual.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
+public class Acesso implements GrantedAuthority {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+	private Long id;
+	
+	@Column(nullable = false)
+	private String descricao; /*Acesso ex: ROLE_ADMIN ou ROLE_SECRETARIO*/
+
+	@Override
+	public String getAuthority() {
+		return this.descricao;
+	}
+}
