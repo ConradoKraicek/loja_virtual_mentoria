ff0e879b67c51e7259a6f13b7a268ad815168f98
Conrado Albuquerque Cardozo <conradoacardoso@gmail.com>
2022-01-10 11:23:53 -0300

    Aula mentoria dia 10-01-2022

--- src/main/java/conra/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/conra/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,16 @@ package conra.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "conra.mentoria.lojavirtual.model")
+@ComponentScan(basePackages = {"conra.*"})
+@EnableJpaRepositories(basePackages = {"conra.mentoria.lojavirtual.repository"})
+@EnableTransactionManagement
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
--- src/test/java/conra/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/conra/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -1,13 +1,35 @@
 package conra.mentoria.lojavirtual;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LojaVirtualMentoriaApplicationTests {
+import conra.mentoria.lojavirtual.controller.AcessoController;
+import conra.mentoria.lojavirtual.model.Acesso;
+import conra.mentoria.lojavirtual.service.AcessoService;
+
+@SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
+public class LojaVirtualMentoriaApplicationTests {
+	
+	@Autowired
+	private AcessoService acessoService;
+	
+	//@Autowired
+	//private AcessoRepository acessoRepository;
+	
+	@Autowired
+	private AcessoController acessoController;
 
 	@Test
-	void contextLoads() {
+	public void testeCadastraAcesso() {
+		
+		Acesso acesso = new Acesso();
+		
+		acesso.setDescricao("ROLE_ADMIN");
+		
+		acessoController.salvarAcesso(acesso);
+		
+		
 	}
 
 }
