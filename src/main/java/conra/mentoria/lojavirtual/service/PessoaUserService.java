ab033bb1dafecfaddadd10deef224f97fc70c4c5
Conrado Albuquerque Cardozo <conradoacardoso@gmail.com>
2022-03-14 16:11:58 -0300

    mentoria comentario

--- src/main/java/conra/mentoria/lojavirtual/service/PessoaUserService.java
+++ src/main/java/conra/mentoria/lojavirtual/service/PessoaUserService.java
@@ -53,6 +53,8 @@ public class PessoaUserService {
 			usuarioPj = usuarioRepository.save(usuarioPj);
 			
 			usuarioRepository.insereAcessoUserPj(usuarioPj.getId());
+			
+			/*Fazer o envio de e-mail do login e da senha*/
 		}
 		
 		return juridica;
