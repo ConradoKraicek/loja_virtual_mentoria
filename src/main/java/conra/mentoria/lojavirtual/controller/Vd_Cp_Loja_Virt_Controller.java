99d050d2919c97ef1f630c958ae5dc39662e586b
Conrado Albuquerque Cardozo <conradoacardoso@gmail.com>
2022-08-24 15:29:01 -0300

    mentoria 24-08-2022

--- src/main/java/conra/mentoria/lojavirtual/controller/Vd_Cp_Loja_Virt_Controller.java
+++ src/main/java/conra/mentoria/lojavirtual/controller/Vd_Cp_Loja_Virt_Controller.java
@@ -5,6 +5,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,8 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import conra.mentoria.lojavirtual.ExceptionMentoriaJava;
 import conra.mentoria.lojavirtual.model.Endereco;
+import conra.mentoria.lojavirtual.model.ItemVendaLoja;
 import conra.mentoria.lojavirtual.model.PessoaFisica;
 import conra.mentoria.lojavirtual.model.VendaCompraLojaVirtual;
+import conra.mentoria.lojavirtual.model.dto.ItemVendaDTO;
 import conra.mentoria.lojavirtual.model.dto.VendaCompraLojaVirtualDTO;
 import conra.mentoria.lojavirtual.repository.EnderecoRepository;
 import conra.mentoria.lojavirtual.repository.NotaFiscalVendaRepository;
@@ -54,6 +58,13 @@ public class Vd_Cp_Loja_Virt_Controller {
 		
 		vendaCompraLojaVirtual.getNotaFiscalVenda().setEmpresa(vendaCompraLojaVirtual.getEmpresa());
 		
+		
+		for (int i = 0; i < vendaCompraLojaVirtual.getItemVendaLojas().size(); i++) {
+			
+			vendaCompraLojaVirtual.getItemVendaLojas().get(i).setEmpresa(vendaCompraLojaVirtual.getEmpresa());
+			vendaCompraLojaVirtual.getItemVendaLojas().get(i).setVendaCompraLojaVirtual(vendaCompraLojaVirtual);
+		}
+		
 		/*Salva primeiro a venda e todos os dados*/
 		vendaCompraLojaVirtual = vd_Cp_Loja_Virt_Repository.saveAndFlush(vendaCompraLojaVirtual);
 		
@@ -65,10 +76,55 @@ public class Vd_Cp_Loja_Virt_Controller {
 		
 		VendaCompraLojaVirtualDTO compraLojaVirtualDTO = new VendaCompraLojaVirtualDTO();
 		compraLojaVirtualDTO.setValorTotal(vendaCompraLojaVirtual.getValorTotal());
+		compraLojaVirtualDTO.setPessoa(vendaCompraLojaVirtual.getPessoa());
+		compraLojaVirtualDTO.setEntrega(vendaCompraLojaVirtual.getEnderecoEntrega());
+		compraLojaVirtualDTO.setCobranca(vendaCompraLojaVirtual.getEnderecoCobranca());
+		compraLojaVirtualDTO.setValorDesc(vendaCompraLojaVirtual.getValorDesconto());
+		compraLojaVirtualDTO.setValorFrete(vendaCompraLojaVirtual.getValorFrete());
+		compraLojaVirtualDTO.setId(vendaCompraLojaVirtual.getId());
+		
+		for (ItemVendaLoja item: vendaCompraLojaVirtual.getItemVendaLojas() ) {
+			
+			ItemVendaDTO itemVendaDTO = new ItemVendaDTO();
+			itemVendaDTO.setQuantidade(item.getQuantidade());
+			itemVendaDTO.setProduto(item.getProduto());
+			
+			compraLojaVirtualDTO.getItemVendaLojas().add(itemVendaDTO);
+		}
+		
+		
 
 		return new ResponseEntity<VendaCompraLojaVirtualDTO>(compraLojaVirtualDTO, HttpStatus.OK); 
 
 
 	}
+	
+	@ResponseBody
+	@GetMapping(value = "**/consultaVendaId/{id}")
+    public ResponseEntity<VendaCompraLojaVirtualDTO> consultaVendaId(@PathVariable("id") Long idVenda) {
+		
+		VendaCompraLojaVirtual vendaCompraLojaVirtual = vd_Cp_Loja_Virt_Repository.findById(idVenda).orElse(new VendaCompraLojaVirtual());
+		
+		VendaCompraLojaVirtualDTO compraLojaVirtualDTO = new VendaCompraLojaVirtualDTO();
+		compraLojaVirtualDTO.setValorTotal(vendaCompraLojaVirtual.getValorTotal());
+		compraLojaVirtualDTO.setPessoa(vendaCompraLojaVirtual.getPessoa());
+		compraLojaVirtualDTO.setEntrega(vendaCompraLojaVirtual.getEnderecoEntrega());
+		compraLojaVirtualDTO.setCobranca(vendaCompraLojaVirtual.getEnderecoCobranca());
+		compraLojaVirtualDTO.setValorDesc(vendaCompraLojaVirtual.getValorDesconto());
+		compraLojaVirtualDTO.setValorFrete(vendaCompraLojaVirtual.getValorFrete());
+		compraLojaVirtualDTO.setId(vendaCompraLojaVirtual.getId());
+		
+        for (ItemVendaLoja item: vendaCompraLojaVirtual.getItemVendaLojas() ) {
+			
+			ItemVendaDTO itemVendaDTO = new ItemVendaDTO();
+			itemVendaDTO.setQuantidade(item.getQuantidade());
+			itemVendaDTO.setProduto(item.getProduto());
+			
+			compraLojaVirtualDTO.getItemVendaLojas().add(itemVendaDTO);
+		}
+		
+		return new ResponseEntity<VendaCompraLojaVirtualDTO>(compraLojaVirtualDTO, HttpStatus.OK); 
+    	
+    }	
 
 }
--- src/main/java/conra/mentoria/lojavirtual/model/dto/VendaCompraLojaVirtualDTO.java
+++ src/main/java/conra/mentoria/lojavirtual/model/dto/VendaCompraLojaVirtualDTO.java
@@ -1,17 +1,94 @@
 package conra.mentoria.lojavirtual.model.dto;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import conra.mentoria.lojavirtual.model.Endereco;
+import conra.mentoria.lojavirtual.model.Pessoa;
 
 public class VendaCompraLojaVirtualDTO {
+	
+	private Long id;
 
 	private BigDecimal valorTotal;
+	
+	private BigDecimal valorDesc;
+
+	private Pessoa pessoa;
+
+	private Endereco cobranca;
+
+	private Endereco entrega;
+	
+	private BigDecimal valorFrete;
+	
+	private List<ItemVendaDTO> itemVendaLojas = new ArrayList<ItemVendaDTO>();
+	
+	
+	
+	public List<ItemVendaDTO> getItemVendaLojas() {
+		return itemVendaLojas;
+	}
+
+	public void setItemVendaLojas(List<ItemVendaDTO> itemVendaLojas) {
+		this.itemVendaLojas = itemVendaLojas;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public BigDecimal getValorTotal() {
 		return valorTotal;
 	}
-
+	
 	public void setValorTotal(BigDecimal valorTotal) {
 		this.valorTotal = valorTotal;
+	}
+
+	public Endereco getCobranca() {
+		return cobranca;
+	}
+
+	public void setCobranca(Endereco cobranca) {
+		this.cobranca = cobranca;
+	}
+
+	public Endereco getEntrega() {
+		return entrega;
+	}
+
+	public void setEntrega(Endereco entrega) {
+		this.entrega = entrega;
+	}
+
+	public BigDecimal getValorDesc() {
+		return valorDesc;
+	}
+	
+	public void setValorDesc(BigDecimal valorDesc) {
+		this.valorDesc = valorDesc;
+	}
+	
+	public BigDecimal getValorFrete() {
+		return valorFrete;
+	}
+
+	public void setValorFrete(BigDecimal valorFrete) {
+		this.valorFrete = valorFrete;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
 	}
 
 }
