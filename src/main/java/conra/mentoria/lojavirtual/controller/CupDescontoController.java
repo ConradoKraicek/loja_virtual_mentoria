ff8c0dd3004a039fd7df0e62d3fe9f75eefaee2a
Conrado Albuquerque Cardozo <conradoacardoso@gmail.com>
2022-11-14 12:12:13 -0300

    mentoria 14-11-2022

--- src/main/java/conra/mentoria/lojavirtual/controller/CupDescontoController.java
+++ src/main/java/conra/mentoria/lojavirtual/controller/CupDescontoController.java
@@ -2,15 +2,22 @@ package conra.mentoria.lojavirtual.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import conra.mentoria.lojavirtual.ExceptionMentoriaJava;
 import conra.mentoria.lojavirtual.model.CupDesc;
+import conra.mentoria.lojavirtual.model.MarcaProduto;
 import conra.mentoria.lojavirtual.repository.CupDescontoRepository;
 
 @RestController
@@ -18,6 +25,41 @@ public class CupDescontoController {
 	
 	@Autowired
 	private CupDescontoRepository cupDescontoRepository;
+	
+	
+	
+	@ResponseBody 
+	@DeleteMapping(value = "**/deleteCupomPorId/{id}") 
+	public ResponseEntity<?> deleteCupomPorId(@PathVariable("id") Long id) {
+		
+		cupDescontoRepository.deleteById(id);
+		
+		return new ResponseEntity("Cupom Removido", HttpStatus.OK);
+	}
+	
+	
+	@ResponseBody 
+	@GetMapping(value = "**/obterCupom/{id}") 
+	public ResponseEntity<CupDesc> obterCupom(@PathVariable("id") Long id) throws ExceptionMentoriaJava {
+		
+		CupDesc cupDesc = cupDescontoRepository.findById(id).orElse(null);
+		
+		if (cupDesc == null) {
+			throw new ExceptionMentoriaJava("Não encontrou Cupom com código:" + id);
+		}
+		
+		return new ResponseEntity<CupDesc>(cupDesc, HttpStatus.OK);
+	}
+	
+	
+	@ResponseBody
+	@PostMapping(value = "**/salvarCupomDesconto")
+	public ResponseEntity<CupDesc> salvarCupomDesconto(@RequestBody @Valid CupDesc cupDesc) throws ExceptionMentoriaJava {
+		
+		CupDesc cupDesc2 = cupDescontoRepository.save(cupDesc);
+		
+		return new ResponseEntity<CupDesc>(cupDesc2, HttpStatus.OK);
+	}
 	
 	
 	@ResponseBody
--- src/main/java/conra/mentoria/lojavirtual/controller/MarcaProdutoController.java
+++ src/main/java/conra/mentoria/lojavirtual/controller/MarcaProdutoController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -56,7 +57,7 @@ public class MarcaProdutoController {
 		return new ResponseEntity("Marca produto Removido", HttpStatus.OK);
 	}
 	
-	
+	//@Secured({"ROLE_GERENTE","ROLE_ADMIN"})
 	@ResponseBody 
 	@DeleteMapping(value = "**/deleteMarcaPorId/{id}") 
 	public ResponseEntity<?> deleteMarcaPorId(@PathVariable("id") Long id) {
