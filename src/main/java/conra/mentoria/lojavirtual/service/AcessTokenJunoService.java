0f2c392f34a8442205f4eb38d94f867d8e54c69f
Conrado Albuquerque Cardozo <conradoacardoso@gmail.com>
2023-02-10 10:57:32 -0300

    mentoria 10-02-2023

--- src/main/java/conra/mentoria/lojavirtual/service/AcessTokenJunoService.java
+++ src/main/java/conra/mentoria/lojavirtual/service/AcessTokenJunoService.java
@@ -4,8 +4,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Service;
+
 import conra.mentoria.lojavirtual.model.AcessTokenJunoAPI;
 
+@Service
 public class AcessTokenJunoService {
 	
 	@PersistenceContext
